701a3a6569a639aa54b7dd7d27aa1af87cb2e896
Christoph Haag <christoph.haag@collabora.com>
2019-11-08 16:59:30 +0100

    get glx context from native context
    drawable is still NULL though

--- src/openxr/stardustopengl.cpp
+++ src/openxr/stardustopengl.cpp
@@ -2,26 +2,32 @@
 
 #include <QCoreApplication>
 #include <QOpenGLContext>
+#include <QtPlatformHeaders/QGLXNativeContext>
+#include <string.h>
 
 namespace Stardust {
 
 OpenGL::OpenGL(QObject *parent) : QObject(parent) {
 
 }
-
 void OpenGL::initialize() {
     //Temporarily make the context current
     QCoreApplication::setAttribute(Qt::AA_DontCheckOpenGLContextThreadAffinity, true);
     window->openglContext()->doneCurrent();
     window->openglContext()->makeCurrent(window);
 
+
     //Get X display
     display = XOpenDisplay(nullptr);
 
-    //Get OpenGL context
-    context = glXGetCurrentContext();
+    QVariant nativeGLHandle = window->openglContext()->nativeHandle();
 
-    drawable = glXGetCurrentDrawable();
+    if (strcmp(nativeGLHandle.typeName(), "QGLXNativeContext") == 0) {
+      QGLXNativeContext nativeContext = qvariant_cast<QGLXNativeContext>(nativeGLHandle);
+      //display = nativeContext.display();
+      context = nativeContext.context();
+      drawable = nativeContext.window();
+    }
 
     int nelements = 0;
     framebufferConfig = glXChooseFBConfig(display, 0, framebufferOptions, &nelements);
