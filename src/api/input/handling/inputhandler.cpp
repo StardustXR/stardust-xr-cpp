3f728731f4cf0e731d30d130407e642cf9d4a686
Nova <technobaboo@gmail.com>
2020-02-22 04:23:18 -0600

    Added ActionTriggers as QML default properties for the InputHandler

--- src/api/input/handling/inputhandler.cpp
+++ src/api/input/handling/inputhandler.cpp
@@ -3,9 +3,35 @@
 namespace StardustAPI {
 namespace Input {
 
-InputHandler::InputHandler(QObject *parent) : QObject(parent)
-{
+InputHandler::InputHandler(QQuick3DNode *parent) : QQuick3DNode(parent) {}
 
+QQmlListProperty<ActionTrigger> InputHandler::qmlActionTriggers()
+{
+    return {this, &actionTriggers,
+             &InputHandler::appendActionTrigger,
+             &InputHandler::actionTriggerCount,
+             &InputHandler::actionTrigger,
+             &InputHandler::clearActionTriggers};
+}
+
+void InputHandler::appendActionTrigger(QQmlListProperty<ActionTrigger> *property, ActionTrigger *p) {
+    InputHandler *handler = qobject_cast<InputHandler *>(property->object);
+    handler->actionTriggers.append(p);
+}
+
+int InputHandler::actionTriggerCount(QQmlListProperty<ActionTrigger> *property) {
+    InputHandler *handler = qobject_cast<InputHandler *>(property->object);
+    return handler->actionTriggers.count();
+}
+
+ActionTrigger *InputHandler::actionTrigger(QQmlListProperty<ActionTrigger> *property, int index) {
+    InputHandler *handler = qobject_cast<InputHandler *>(property->object);
+    return handler->actionTriggers.at(index);
+}
+
+void InputHandler::clearActionTriggers(QQmlListProperty<ActionTrigger> *property) {
+    InputHandler *handler = qobject_cast<InputHandler *>(property->object);
+    handler->actionTriggers.clear();
 }
 
 }
--- src/api/input/handling/inputhandler.h
+++ src/api/input/handling/inputhandler.h
@@ -1,7 +1,8 @@
 #ifndef STARDUSTAPI_INPUT_INPUTHANDLER_H
 #define STARDUSTAPI_INPUT_INPUTHANDLER_H
 
-#include <QObject>
+#include <QtQuick3D/private/qquick3dnode_p.h>
+#include <QQmlListProperty>
 #include "actiontrigger.h"
 #include "../methods/input.h"
 #include "../../fields/field.h"
@@ -9,11 +10,15 @@
 namespace StardustAPI {
 namespace Input {
 
-class InputHandler : public QObject
+class InputHandler : public QQuick3DNode
 {
     Q_OBJECT
+    Q_PROPERTY(float maxDistance MEMBER maxDistance)
+    Q_PROPERTY(Field *field MEMBER field)
+    Q_PROPERTY(QQmlListProperty<StardustAPI::Input::ActionTrigger> actionTriggers READ qmlActionTriggers)
+    Q_CLASSINFO("DefaultProperty", "actionTriggers")
 public:
-    explicit InputHandler(QObject *parent = nullptr);
+    explicit InputHandler(QQuick3DNode *parent = nullptr);
 
     float maxDistance = 0.01f;
     Field *field = nullptr;
@@ -21,7 +26,13 @@ public:
     bool rejectAction = false;
 
     QList<ActionTrigger *> actionTriggers;
+    QQmlListProperty<ActionTrigger> qmlActionTriggers();
 
+private:
+    static void appendActionTrigger(QQmlListProperty<ActionTrigger>*, ActionTrigger *p);
+    static int actionTriggerCount(QQmlListProperty<ActionTrigger>*);
+    static ActionTrigger *actionTrigger(QQmlListProperty<ActionTrigger>*, int index);
+    static void clearActionTriggers(QQmlListProperty<ActionTrigger>*);
 };
 
 }
