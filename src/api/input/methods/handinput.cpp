3cae86675bfc767ed7748a27ebf27d89d2c147b0
Nova <technobaboo@gmail.com>
2020-03-03 06:17:29 -0600

    Added HandBone type

--- src/api/input/methods/handinput.cpp
+++ src/api/input/methods/handinput.cpp
@@ -7,5 +7,37 @@ float HandInput::distanceToField(StardustAPI::Fields::Field *field) {
     return field->distance(this->mapPositionToScene(localInteractPoint));
 }
 
+HandInputBone::HandInputBone(HandInputBone *parent) : QQuick3DNode(parent), parentBone(parent) {
+
+}
+
+QQmlListProperty<HandInputBone> HandInputBone::qmlChildBones() {
+    return {this, &childBones,
+             &HandInputBone::appendChildBone,
+             &HandInputBone::childBoneCount,
+             &HandInputBone::childBone,
+                &HandInputBone::clearChildBones};
+}
+
+void HandInputBone::appendChildBone(QQmlListProperty<HandInputBone> *property, HandInputBone *p) {
+    HandInputBone *bone = qobject_cast<HandInputBone *>(property->object);
+    bone->childBones.append(p);
+}
+
+int HandInputBone::childBoneCount(QQmlListProperty<HandInputBone> *property) {
+    HandInputBone *bone = qobject_cast<HandInputBone *>(property->object);
+    return bone->childBones.length();
+}
+
+HandInputBone *HandInputBone::childBone(QQmlListProperty<HandInputBone> *property, int index) {
+    HandInputBone *bone = qobject_cast<HandInputBone *>(property->object);
+    return bone->childBones.at(index);
+}
+
+void HandInputBone::clearChildBones(QQmlListProperty<HandInputBone> *property) {
+    HandInputBone *bone = qobject_cast<HandInputBone *>(property->object);
+    bone->childBones.clear();
+}
+
 }
 }
--- src/api/input/methods/handinput.h
+++ src/api/input/methods/handinput.h
@@ -6,6 +6,30 @@
 namespace StardustAPI {
 namespace Input {
 
+class HandInputBone : public QQuick3DNode {
+    Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<StardustAPI::Input::HandInputBone> childBones READ qmlChildBones)
+    Q_CLASSINFO("DefaultProperty", "childBones")
+public:
+    explicit HandInputBone(HandInputBone *parent = nullptr);
+
+    float width;
+    float length();
+
+    QVector3D parentJoint();
+    QVector3D direction();
+
+    HandInputBone *parentBone;
+    QList<HandInputBone *> childBones;
+    QQmlListProperty<HandInputBone> qmlChildBones();
+
+private:
+    static void appendChildBone(QQmlListProperty<HandInputBone>*, HandInputBone *p);
+    static int childBoneCount(QQmlListProperty<HandInputBone>*);
+    static HandInputBone *childBone(QQmlListProperty<HandInputBone>*, int index);
+    static void clearChildBones(QQmlListProperty<HandInputBone>*);
+};
+
 class HandInput : public Input {
     Q_OBJECT
 public:
