3088005ea3685ee181838803af1c7cfabeda31d2
Nova <technobaboo@gmail.com>
2020-07-15 01:51:08 -0500

    Removed all python scripts

--- stardust/scripts/ipc/stardust_messenger.py
+++
@@ -1,9 +0,0 @@
-import os
-import threading
-from time import sleep
-from stardust_xr import messenger
-from godot import exposed, export, Node, NodePath
-
-@exposed
-class stardust_messenger(Node, Messenger):
-	pass
