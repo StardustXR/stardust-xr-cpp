ecbc04f758e75fbd27dee294345bc18fd27982bb
Nova <technobaboo@gmail.com>
2020-11-26 06:10:39 -0600

    Adapted field.cpp to use new space/local transforms

--- src/nodetypes/fields/field.cpp
+++ src/nodetypes/fields/field.cpp
@@ -20,22 +20,19 @@ const vec3 Field::localNormal(const vec3 point) {
 
 const vec3 Field::localNormal(const vec3 point, float radius) {
 	float d = localDistance(point);
-		vec2 e = { radius, 0.0f };
+	vec2 e = { radius, 0.0f };
 
-		vec3 n = vec3{ d,d,d } - vec3{
-			localDistance(vec3{e.x, e.y, e.y}),
-			localDistance(vec3{e.y, e.x, e.y}),
-			localDistance(vec3{e.y, e.y, e.x})
-		};
+	vec3 n = vec3{ d,d,d } - vec3{
+		localDistance(vec3{e.x, e.y, e.y}),
+		localDistance(vec3{e.y, e.x, e.y}),
+		localDistance(vec3{e.y, e.y, e.x})
+	};
 
-		return vec3_normalize(n);
+	return vec3_normalize(n);
 }
 
-float Field::distance(const vec3 point) {
-	cleanTransform();
-	matrix inverse_transform_matrix;
-	matrix_inverse(transform, inverse_transform_matrix);
-	vec3 localPoint = matrix_mul_point(inverse_transform_matrix, point);
+float Field::distance(SpatialNode *space, const vec3 point) {
+	vec3 localPoint = spaceToLocalPoint(space, point);
 
 	if(scale.x == 1.0f && scale.x == scale.y && scale.y == scale.z) { // We should really fix this but idk how??
 		//If the field is not scaled
@@ -49,29 +46,24 @@ float Field::distance(const vec3 point) {
 		vec3 localSurfacePoint = localClosestPoint(localPoint);
 		float localScaledDistance = localDistance(localPoint); //Get the distance so we know if the point is inside or outside
 
-		vec3 surfacePoint = matrix_mul_point(transform, localSurfacePoint);
+		vec3 surfacePoint = localToSpacePoint(space, localSurfacePoint);
 		float distance = vec3_magnitude(point - surfacePoint);
 		return copysignf(distance, localScaledDistance); //Apply the sign of the DE to tell if the point is inside/outside
 	}
 }
 
-const vec3 Field::closestPoint(const vec3 point) {
-	cleanTransform();
-	matrix inverse_transform_matrix;
-	matrix_inverse(transform, inverse_transform_matrix);
-	vec3 localPoint = matrix_mul_point(inverse_transform_matrix, point);
-	return matrix_mul_point(transform, localClosestPoint(localPoint));
+const vec3 Field::closestPoint(SpatialNode *space, const vec3 point) {
+	vec3 localPoint = spaceToLocalPoint(space, point);
+	return localToSpaceDirection(space, localClosestPoint(localPoint));
 }
 
-const vec3 Field::normal(const vec3 point) {
-	return normal(point, normalRadius);
+const vec3 Field::normal(SpatialNode *space, const vec3 point) {
+	return normal(space, point, normalRadius);
 }
 
-const vec3 Field::normal(const vec3 point, float radius) {
-	matrix inverse_transform_matrix;
-	matrix_inverse(transform, inverse_transform_matrix);
-	vec3 localPoint = matrix_mul_point(inverse_transform_matrix, point);
-	return matrix_mul_direction(transform, localNormal(localPoint, radius));
+const vec3 Field::normal(SpatialNode *space, const vec3 point, float radius) {
+	vec3 localPoint = spaceToLocalPoint(space, point);
+	return localToSpaceDirection(space, localNormal(localPoint, radius));
 }
 
 } // namespace StardustXRServer
