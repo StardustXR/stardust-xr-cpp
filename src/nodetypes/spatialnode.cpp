768fce5970825e25075e23dd9210301fe61ec756
Nova <technobaboo@gmail.com>
2020-11-25 23:06:39 -0600

    Added setPose and setTransform to SpatialNode

--- src/nodetypes/spatialnode.cpp
+++ src/nodetypes/spatialnode.cpp
@@ -6,6 +6,9 @@ SpatialNode::SpatialNode() {
 	STARDUSTXR_NODE_METHOD("setPosition", &SpatialNode::setPosition)
 	STARDUSTXR_NODE_METHOD("setRotation", &SpatialNode::setRotation)
 	STARDUSTXR_NODE_METHOD("setScale", &SpatialNode::setScale)
+
+	STARDUSTXR_NODE_METHOD("setPose", &SpatialNode::setPose)
+	STARDUSTXR_NODE_METHOD("setTransform", &SpatialNode::setTransform)
 }
 
 std::vector<uint8_t> SpatialNode::setPosition(uint sessionID, flexbuffers::Reference data, bool returnValue) {
@@ -34,6 +37,24 @@ std::vector<uint8_t> SpatialNode::setScale(uint sessionID, flexbuffers::Referenc
 		scale = { vector[0].AsFloat(), vector[1].AsFloat(), vector[2].AsFloat() };
 		transformMatrixDirty = true;
 	}
+
+	return FlexbufferFromArguments([](flexbuffers::Builder &fbb) { fbb.Null(); });
+}
+
+std::vector<uint8_t> SpatialNode::setPose(uint sessionID, flexbuffers::Reference data, bool returnValue) {
+	flexbuffers::Vector vector = data.AsVector();
+	setPosition(sessionID, vector[0], false);
+	setRotation(sessionID, vector[1], false);
+	setScale(sessionID, vector[2], false);
+
+	return FlexbufferFromArguments([](flexbuffers::Builder &fbb) { fbb.Null(); });
+}
+
+std::vector<uint8_t> SpatialNode::setTransform(uint sessionID, flexbuffers::Reference data, bool returnValue) {
+	flexbuffers::Vector vector = data.AsVector();
+	setPosition(sessionID, vector[0], false);
+	setRotation(sessionID, vector[1], false);
+	setScale(sessionID, vector[2], false);
 
 	return FlexbufferFromArguments([](flexbuffers::Builder &fbb) { fbb.Null(); });
 }
--- src/nodetypes/spatialnode.hpp
+++ src/nodetypes/spatialnode.hpp
@@ -18,6 +18,9 @@ public:
 	std::vector<uint8_t> setRotation(uint sessionID, flexbuffers::Reference data, bool returnValue);
 	std::vector<uint8_t> setScale(uint sessionID, flexbuffers::Reference data, bool returnValue);
 
+	std::vector<uint8_t> setPose(uint sessionID, flexbuffers::Reference data, bool returnValue);
+	std::vector<uint8_t> setTransform(uint sessionID, flexbuffers::Reference data, bool returnValue);
+
 	vec3 position = vec3_zero;
 	quat rotation = quat_identity;
 	vec3 scale = vec3_one;
