18e5d7ac02ff20e14569d249bb80aa44754bcc94
Nova <technobaboo@gmail.com>
2020-11-26 06:00:20 -0600

    Cleaned up spatialnode.hpp

--- src/nodetypes/spatialnode.hpp
+++ src/nodetypes/spatialnode.hpp
@@ -15,17 +15,20 @@ public:
 	virtual ~SpatialNode() {}
 
 	// Client accessible functions
-	std::vector<uint8_t> setPosition(uint sessionID, flexbuffers::Reference data, bool returnValue);
-	std::vector<uint8_t> setRotation(uint sessionID, flexbuffers::Reference data, bool returnValue);
-	std::vector<uint8_t> setScale(uint sessionID, flexbuffers::Reference data, bool returnValue);
-	std::vector<uint8_t> setPose(uint sessionID, flexbuffers::Reference data, bool returnValue);
+	std::vector<uint8_t> setPosition (uint sessionID, flexbuffers::Reference data, bool returnValue);
+	std::vector<uint8_t> setRotation (uint sessionID, flexbuffers::Reference data, bool returnValue);
+	std::vector<uint8_t> setScale    (uint sessionID, flexbuffers::Reference data, bool returnValue);
+	std::vector<uint8_t> setPose     (uint sessionID, flexbuffers::Reference data, bool returnValue);
 	std::vector<uint8_t> setTransform(uint sessionID, flexbuffers::Reference data, bool returnValue);
 
 	// Surface level parameters
 	vec3 position = vec3_zero;
 	quat rotation = quat_identity;
-	vec3 scale = vec3_one;
+	vec3 scale    = vec3_one;
 
+	// Matrix get operations
+	matrix localTransform();
+	matrix worldTransform();
 	// Spatial heirarchy relationships
 	SpatialNode *spatialParent = nullptr;
 
@@ -33,9 +36,6 @@ public:
 protected:
 	bool transformMatrixDirty = false;
 	matrix transform = matrix_identity;
-
-	matrix localTransform();
-	matrix worldTransform();
 };
 
 } // namespace StardustXRServer
