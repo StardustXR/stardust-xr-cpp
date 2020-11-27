a27ab0d558250e36b6752d867e1e8983e814223c
Nova <technobaboo@gmail.com>
2020-11-27 06:49:15 -0600

    Created ThreadSafeList

---
+++ src/util/threadsafelist.cpp
@@ -0,0 +1,119 @@
+#include "threadsafelist.hpp"
+#include <cmath>
+
+namespace StardustXRServer {
+
+template<class T>
+ThreadSafeList<T>::ThreadSafeList() {
+	pthread_mutex_init(&lockMutex, nullptr);
+}
+
+template<class T>
+ThreadSafeList<T>::~ThreadSafeList() {
+	pthread_mutex_destroy(&lockMutex);
+}
+
+template<class T>
+uint32_t ThreadSafeList<T>::length() {
+	tryLock();
+	if(!lengthDirty)
+		return listLength;
+
+	listLength = (bool) begin;
+	ListItem *currentItem = begin;
+
+	for(; currentItem->next != nullptr; listLength++)
+		currentItem = currentItem->next;
+
+	return listLength;
+}
+
+template<class T>
+void ThreadSafeList<T>::pushFront(const T &object) {
+	tryLock();
+	lengthDirty = true;
+	ListItem *newItem = new ListItem();
+	newItem->value = new T(object);
+	newItem->next = begin;
+	begin = newItem;
+	if(end == nullptr)
+		end = begin;
+}
+
+template<class T>
+void ThreadSafeList<T>::pushBack(const T &object) {
+	tryLock();
+	lengthDirty = true;
+	ListItem *newItem = new ListItem();
+	newItem->value = new T(object);
+	newItem->previous = end;
+	end = newItem;
+	if(begin == nullptr)
+		begin = end;
+}
+
+template<class T>
+void ThreadSafeList<T>::erase(int index) {
+	tryLock();
+	lengthDirty = true;
+	ListItem *item = get(index);
+
+	if(item == begin)
+		begin = item->next;
+	else
+		item->previous->next = item->next;
+
+	if(item == end)
+		end = item->previous;
+	else
+		item->next->previous = item->previous;
+
+	delete item;
+}
+
+template<class T>
+bool ThreadSafeList<T>::exists(int index) {
+	return (index >= 0) ? index+1 : -index > length();
+}
+
+template<class T>
+T &ThreadSafeList<T>::at(int i) {
+	tryLock();
+	return *get(i).value;
+}
+
+template<class T>
+T &ThreadSafeList<T>::operator[](int i) {
+	return at(i);
+}
+
+template<class T>
+void ThreadSafeList<T>::done() {
+	if(locked) {
+		locked = false;
+		pthread_mutex_unlock(&lockMutex);
+	}
+}
+
+template<class T>
+void ThreadSafeList<T>::tryLock() {
+	if(!locked) {
+		locked = true;
+		pthread_mutex_lock(&lockMutex);
+	}
+}
+
+template<class T>
+typename ThreadSafeList<T>::ListItem *ThreadSafeList<T>::get(int index) {
+	tryLock();
+
+	int iMax = (index >= 0) ? index : length() + index;
+	ListItem *currentItem = begin;
+
+	for(int i=1; i<iMax; ++i)
+		currentItem = currentItem->next;
+
+	return currentItem;
+}
+
+} // namespace StardustXRServer
---
+++ src/util/threadsafelist.hpp
@@ -0,0 +1,50 @@
+#ifndef STARDUSTXRSERVER_THREADSAFELIST_HPP
+#define STARDUSTXRSERVER_THREADSAFELIST_HPP
+
+#include <pthread.h>
+#include <stddef.h>
+#include <inttypes.h>
+
+namespace StardustXRServer {
+
+template <class T>
+class ThreadSafeList {
+public:
+	ThreadSafeList();
+	~ThreadSafeList();
+
+	uint32_t length();
+	bool exists(int index);
+
+	T &operator[](int i);
+	T &at(int i);
+
+	void pushFront(const T &object);
+	void pushBack(const T &object);
+	void erase(int index);
+
+	void done();
+
+protected:
+	pthread_mutex_t lockMutex;
+	bool locked;
+	void tryLock();
+
+	struct ListItem {
+		struct ListItem *previous;
+		T *value;
+		struct ListItem *next;
+	};
+	typedef struct ListItem ListItem;
+
+	ListItem *begin = nullptr;
+	ListItem *end = nullptr;
+
+	uint32_t listLength;
+	bool lengthDirty;
+	ListItem *get(int index);
+};
+
+} // namespace StardustXRServer
+
+#endif // STARDUSTXRSERVER_THREADSAFELIST_HPP
