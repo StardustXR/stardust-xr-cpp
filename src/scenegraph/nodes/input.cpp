fdb7d7668e89f9e760043f25eab8e328d151c391
Nova <technobaboo@gmail.com>
2020-12-19 22:37:06 -0600

    Added initial Input node

---
+++ src/scenegraph/nodes/input.cpp
@@ -0,0 +1,35 @@
+#include "input.hpp"
+#include "../../globals.h"
+
+namespace StardustXRServer {
+
+InputInterface::InputInterface() {
+	children["handler"] = new Node();
+	children["handler"]->parent = this;
+
+	children["method"] = new Node();
+	children["method"]->parent = this;
+
+	STARDUSTXR_NODE_METHOD("registerInputHandler", &InputInterface::registerInputHandler)
+}
+InputInterface::~InputInterface() {}
+
+std::vector<uint8_t> InputInterface::registerInputHandler(uint sessionID, flexbuffers::Reference data, bool) {
+	flexbuffers::Vector vec    = data.AsVector();
+	std::string name           = vec[0].AsString().str();
+	std::string field          = vec[1].AsString().str();
+	std::string callbackPath   = vec[2].AsString().str();
+	std::string callbackMethod = vec[3].AsString().str();
+
+	InputHandler *handler              = new InputHandler();
+	handler->ready                     = false;
+	handler->parent                    = this;
+	children["method"]->children[name] = handler;
+	handler->sessionID                 = sessionID;
+	handler->field                     = dynamic_cast<Field *>(scenegraph.findNode(field));
+	handler->callbackPath              = callbackPath;
+	handler->callbackMethod            = callbackMethod;
+	handler->ready                     = true;
+}
+
+} // namespace StardustXRServer
---
+++ src/scenegraph/nodes/input.hpp
@@ -0,0 +1,20 @@
+#ifndef STARDUSTXRSERVER_INPUTINTERFACE_HPP
+#define STARDUSTXRSERVER_INPUTINTERFACE_HPP
+
+#include "../../nodetypes/node.hpp"
+#include "../../nodetypes/input/inputhandler.hpp"
+#include "../../nodetypes/input/inputmethod.hpp"
+
+namespace StardustXRServer {
+
+class InputInterface : public Node {
+public:
+	InputInterface();
+	~InputInterface();
+
+	std::vector<uint8_t> registerInputHandler(uint sessionID, flexbuffers::Reference data, bool returnValue);
+};
+
+} // namespace StardustXRServer
+
+#endif // STARDUSTXRSERVER_INPUTINTERFACE_HPP
