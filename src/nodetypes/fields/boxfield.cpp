e7004a688e4d4ec4ec5ea332f6771b6f0585e213
Nova <technobaboo@gmail.com>
2021-02-26 17:45:33 -0600

    Updated box field SDF to perfect formula

--- src/nodetypes/fields/boxfield.cpp
+++ src/nodetypes/fields/boxfield.cpp
@@ -8,20 +8,18 @@ namespace StardustXRServer {
 BoxField::BoxField() {}
 
 float BoxField::localDistance(const vec3 point) {
-	vec3 offsetPoint = localToSpacePoint(nullptr, point);
-	offsetPoint.x = abs(offsetPoint.x) - (size.x / 2.0f);
-	offsetPoint.y = abs(offsetPoint.y) - (size.y / 2.0f);
-	offsetPoint.z = abs(offsetPoint.z) - (size.z / 2.0f);
+	vec3 q = {
+		std::abs(point.x) - (size.x * 0.5f),
+		std::abs(point.y) - (size.y * 0.5f),
+		std::abs(point.z) - (size.z * 0.5f)
+	};
+	vec3 v = {
+		std::max(q.x, 0.0f),
+		std::max(q.y, 0.0f),
+		std::max(q.z, 0.0f)
+	};
 
-	vec3 outsideVector = { 0.0f, 0.0f, 0.0f };
-	outsideVector.x = max(offsetPoint.x, 0.0f);
-	outsideVector.x = max(offsetPoint.y, 0.0f);
-	outsideVector.x = max(offsetPoint.z, 0.0f);
-
-	float outsideDistance = vec3_magnitude(outsideVector);
-	float insideDistance = min(max(offsetPoint.x, max(offsetPoint.y, offsetPoint.z)), 0.0f);
-
-	return outsideDistance + insideDistance;
+	return vec3_magnitude(v) + std::min(std::max(q.x, std::max(q.y, q.z)), 0.0f);
 }
 
 } // namespace StardustXRServer
