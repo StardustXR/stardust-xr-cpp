2143f280e97365611df554c82d960aa8018b4541
Nova <technobaboo@gmail.com>
2021-05-10 22:42:11 -0500

    Added child name caching in node propagation method

--- src/nodetypes/node.cpp
+++ src/nodetypes/node.cpp
@@ -1,12 +1,22 @@
 #include "node.hpp"
+#include <vector>
+
+using namespace std;
 
 namespace StardustXRServer {
 
 void Node::propagate(std::string name, PropagateFunction &function) {
 	if(ready && function(name, this)) {
-		for(const auto &child : children) {
-			if(child.second)
-				child.second->propagate(child.first, function);
+		// Cache names of all children
+		vector<string> childNames;
+		for(const auto &child : children)
+			childNames.push_back(child.first);
+
+		// Delete children using name cache
+		for(const auto &childName : childNames) {
+			Node *child = children.at(childName);
+			if(child)
+				child->propagate(childName, function);
 
 			if(children.size() == 0)
 				break;
