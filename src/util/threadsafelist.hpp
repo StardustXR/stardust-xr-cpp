08b991357b5b8e884927aae1ab9d90e55e53194f
Nova <technobaboo@gmail.com>
2021-05-26 08:36:21 -0500

    Moved all threadsafe list implementation into main header

--- src/util/threadsafelist.hpp
+++ src/util/threadsafelist.hpp
@@ -11,30 +11,125 @@ namespace StardustXRServer {
 template <class T>
 class ThreadSafeList {
 public:
-	ThreadSafeList();
-	~ThreadSafeList();
+	ThreadSafeList() {
+		pthread_mutex_init(&lockMutex, nullptr);
+	}
+	~ThreadSafeList() {
+		pthread_mutex_destroy(&lockMutex);
+	}
 
-	uint32_t length();
-	bool exists(int index);
+	uint32_t length() {
+		tryLock();
+		if(!lengthDirty)
+			return listLength;
 
-	T &operator[](int i);
-	T &at(int i);
+		listLength = (bool) begin;
+		ListItem *currentItem = begin;
+
+		for(; currentItem && currentItem->next != nullptr; listLength++)
+			currentItem = currentItem->next;
+
+		return listLength;
+	}
+	bool exists(int index) {
+		return (index >= 0) ? index+1 : -index > length();
+	}
+
+	T &operator[](int i) {
+		return at(i);
+	}
+	T &at(int i) {
+		tryLock();
+		return *get(i)->value;
+	}
 
 	typedef std::function<void(uint32_t, T &)> ForEachFunction;
-	void forEach(ForEachFunction function);
+	void forEach(ForEachFunction function) {
+		tryLock();
 
-	void pushFront(const T &object);
-	void pushBack(const T &object);
-	void erase(int index);
+		uint32_t i = 0;
+		ListItem *currentItem = begin;
+		while(currentItem) {
+			ListItem *nextItem = currentItem->next;
+			function(i, *currentItem->value);
+			currentItem = nextItem;
+			i++;
+		}
+	}
 
-	void done();
+	void pushFront(const T &object) {
+		tryLock();
+		lengthDirty = true;
+		ListItem *newItem = new ListItem();
+		newItem->value = new T(object);
+		newItem->next = begin;
+		if(begin)
+			begin->previous = newItem;
+		begin = newItem;
+		if(end == nullptr)
+			end = begin;
+	}
+	void pushBack(const T &object) {
+		tryLock();
+		lengthDirty = true;
+		ListItem *newItem = new ListItem();
+		newItem->value = new T(object);
+		newItem->previous = end;
+		if(end)
+			end->next = newItem;
+		end = newItem;
+		if(begin == nullptr)
+			begin = end;
+	}
+	void erase(int index) {
+		tryLock();
+		lengthDirty = true;
+		ListItem *item = get(index);
+
+		if(item == begin)
+			begin = item->next;
+		else if(item->previous)
+			item->previous->next = item->next;
+
+		if(item == end)
+			end = item->previous;
+		else if(item->next)
+			item->next->previous = item->previous;
+
+		delete item->value;
+		delete item;
+	}
+
+	void done() {
+		pthread_t thisThread = pthread_self();
+		int lockedPosition = lockPosition(thisThread);
+		if(lockedPosition >= 0) {
+			lockedThreads.erase(lockedThreads.begin()+lockedPosition);
+			pthread_mutex_unlock(&lockMutex);
+		}
+	}
 
 protected:
 	pthread_mutex_t lockMutex;
 	std::vector<pthread_t> lockedThreads;
-	void tryLock();
-	bool isLocked(pthread_t &currentThread);
-	int lockPosition(pthread_t &currentThread);
+	void tryLock() {
+		pthread_t thisThread = pthread_self();
+		if(!isLocked(thisThread)) {
+			lockedThreads.push_back(thisThread);
+			pthread_mutex_lock(&lockMutex);
+		}
+	}
+	bool isLocked(pthread_t &currentThread) {
+		return lockPosition(currentThread) != -1;
+	}
+	int lockPosition(pthread_t &currentThread) {
+		for(uint32_t i=0; i<lockedThreads.size(); ++i) {
+			if(pthread_equal(lockedThreads[i], currentThread)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 	struct ListItem {
 		struct ListItem *previous;
@@ -48,9 +143,17 @@ protected:
 
 	uint32_t listLength;
 	bool lengthDirty;
-	ListItem *get(int index);
+	ListItem *get(int index) {
+		tryLock();
+
+		int iMax = (index >= 0) ? index : length() + index;
+		ListItem *currentItem = begin;
+
+		for(int i=0; i<iMax; ++i)
+			currentItem = currentItem->next;
+
+		return currentItem;
+	}
 };
 
 } // namespace StardustXRServer
-
-#include "threadsafelist_impl.hpp"
