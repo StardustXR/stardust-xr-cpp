c0570012cdd7c06ca2c324fb401667ed0f7a3868
technobaboo <technobaboo@gmail.com>
2021-07-07 20:31:55 -0500

    Add setVisible DrawableNode method

--- src/nodetypes/graphical/drawablenode.hpp
+++ src/nodetypes/graphical/drawablenode.hpp
@@ -6,10 +6,15 @@ namespace StardustXRServer {
 
 class DrawableNode : public SpatialNode {
 public:
-	DrawableNode() {}
+	DrawableNode() {
+		STARDUSTXR_NODE_METHOD("setVisible", &DrawableNode::setVisible)
+	}
 	virtual ~DrawableNode() {}
 
 	bool visible = true;
+	std::vector<uint8_t> setVisible(uint sessionID, flexbuffers::Reference data, bool returnValue) {
+		visible = data.AsBool();
+	}
 	virtual void draw() {}
 };
 
