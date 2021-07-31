fb16488fb717d913dff8676e8c080275206071a7
technobaboo <technobaboo@gmail.com>
2021-07-31 12:50:32 -0500

    Properly queue all material property changes

--- src/nodetypes/graphical/model.cpp
+++ src/nodetypes/graphical/model.cpp
@@ -12,12 +12,41 @@ Model::Model(Client *client, std::string modelPath, Spatial *spatialParent, vec3
 	STARDUSTXR_NODE_METHOD("setMaterialProperty", &Model::setMaterialProperty);
 }
 
+Model::~Model() {
+	model_release(model);
+}
+
 void Model::update() {
-	if(queued) {
-		queued = false;
-		model = model_create_file(modelPath.c_str());
+	if(modelQueued) {
+		modelQueued = false;
+		model_t original = model_create_file(modelPath.c_str());
+		model = model_copy(original);
+		model_release(original);
 		visible = true;
 	}
+
+	for(MaterialProperty &prop : queuedProperties) {
+		material_t originalMat = model_get_material(model, prop.submeshIndex);
+		auto modifiedMat = std::find(modifiedMaterials.begin(), modifiedMaterials.end(), originalMat);
+		material_t mat;
+		if(modifiedMat != modifiedMaterials.end()) {
+			mat = *modifiedMat;
+		} else {
+			mat = material_copy(originalMat);
+			model_set_material(model, prop.submeshIndex, mat);
+		}
+
+		if(prop.isFloat)
+			material_set_float(mat, prop.name.c_str(), prop.floatValue);
+		if(prop.isColor)
+			material_set_color(mat, prop.name.c_str(), prop.colorValue);
+		if(prop.isString) {
+			tex_t tex = tex_create_file(prop.stringValue.c_str());
+			if(material_set_texture(mat, prop.name.c_str(), tex))
+				tex_release(tex);
+		}
+	}
+	queuedProperties.clear();
 }
 
 void Model::draw() {
@@ -39,21 +68,33 @@ std::vector<uint8_t> Model::setMaterialProperty(flexbuffers::Reference data, boo
 
 	material_t mat = model_get_material(model, submeshIndex);
 
-	if(propertyValue.IsFloat()) {
-		material_set_float(mat, propertyName, propertyValue.AsFloat());
-	} else if (propertyValue.IsTypedVector()) {
+	MaterialProperty prop = {};
+	prop.submeshIndex = submeshIndex;
+	prop.name = propertyName;
+
+	prop.isFloat = propertyValue.IsFloat();
+	prop.isColor = propertyValue.IsTypedVector();
+	prop.isString = propertyValue.IsString();
+
+	if(prop.isFloat) {
+		prop.floatValue = propertyValue.AsFloat();
+	} else if (prop.isColor) {
 		flexbuffers::TypedVector flexColor = propertyValue.AsTypedVector();
 		if(flexColor.size() == 4 && flexColor[0].IsFloat()) {
-			material_set_color(mat, propertyName, color128{
+			prop.colorValue = color128{
 				flexColor[0].AsFloat(),
 				flexColor[1].AsFloat(),
 				flexColor[2].AsFloat(),
 				flexColor[3].AsFloat()
-			});
+			};
+		} else {
+			return std::vector<uint8_t>();
 		}
-	} else if (propertyValue.IsString()) {
-		material_set_texture(mat, propertyName, tex_create_file(propertyValue.AsString().c_str()));
+	} else if (prop.isString) {
+		prop.stringValue = propertyValue.AsString().str();
 	}
+
+	queuedProperties.push_back(prop);
 
 	return std::vector<uint8_t>();
 };
--- src/nodetypes/graphical/model.hpp
+++ src/nodetypes/graphical/model.hpp
@@ -1,23 +1,43 @@
 #pragma once
 
 #include "drawablenode.hpp"
+#include <string>
 #include <stereokit.h>
+#include <vector>
 
 namespace StardustXRServer {
 
 class Model : public DrawableNode {
 public:
 	Model(Client *client, std::string modelPath, Spatial *spatialParent, sk::vec3 position, sk::quat rotation, sk::vec3 scale);
-	virtual ~Model() {}
+	virtual ~Model();
 
 	void update();
 	void draw();
 
 	std::vector<uint8_t> setMaterialProperty(flexbuffers::Reference data, bool returnValue);
 
-	bool queued = true;
+protected:
 	std::string modelPath;
 	sk::model_t model = nullptr;
+	bool modelQueued = true;
+
+	struct MaterialProperty {
+		std::string name = "";
+		uint32_t submeshIndex = 0;
+
+		bool isFloat;
+		float floatValue = 0;
+
+		bool isColor;
+		color128 colorValue = {0,0,0,0};
+
+		bool isString;
+		std::string stringValue = "";
+	};
+
+	std::vector<MaterialProperty> queuedProperties;
+	std::vector<sk::material_t> modifiedMaterials;
 };
 
 }
