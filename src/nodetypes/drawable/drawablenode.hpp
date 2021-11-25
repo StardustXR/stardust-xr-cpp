cb60acd27a7bd15c6a1093e625d5547ce66b1a0d
Nova <technobaboo@gmail.com>
2021-11-25 13:50:14 -0600

    Remove visible property from Drawable

--- src/nodetypes/drawable/drawablenode.hpp
+++ src/nodetypes/drawable/drawablenode.hpp
@@ -7,16 +7,8 @@ namespace StardustXRServer {
 
 class DrawableNode : public Spatial {
 public:
-	DrawableNode(Client *client, Spatial *spatialParent, sk::vec3 position, sk::quat rotation, sk::vec3 scale, bool translatable, bool rotatable, bool scalable) : Spatial(client, spatialParent, position, rotation, scale, translatable, rotatable, scalable, false) {
-		STARDUSTXR_NODE_METHOD("setVisible", &DrawableNode::setVisible)
-	}
+	DrawableNode(Client *client, Spatial *spatialParent, sk::vec3 position, sk::quat rotation, sk::vec3 scale, bool translatable, bool rotatable, bool scalable) : Spatial(client, spatialParent, position, rotation, scale, translatable, rotatable, scalable, false) {}
 	virtual ~DrawableNode() {}
-
-	bool visible = true;
-	std::vector<uint8_t> setVisible(flexbuffers::Reference data, bool returnValue) {
-		visible = data.AsBool();
-		return std::vector<uint8_t>();
-	}
 	virtual void draw() {}
 };
 
