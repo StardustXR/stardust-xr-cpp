ec7be6b805b88267784b8355f1ec6f7920ee0887
Nova <technobaboo@gmail.com>
2022-01-11 11:42:28 -0500

    Fix panel alias submesh index

--- src/nodetypes/items/alias.cpp
+++ src/nodetypes/items/alias.cpp
@@ -14,7 +14,7 @@ PanelAlias::PanelAlias(Client *client, PanelItem *original, std::vector<std::str
 std::vector<uint8_t> PanelAlias::applySurfaceMaterial(flexbuffers::Reference data, bool returnValue) {
 	flexbuffers::Vector flexVec      = data.AsVector();
 	Model *model				     = this->client->scenegraph.findNode<Model>(flexVec[0].AsString().str());
-	uint32_t submeshIndex            = flexVec[0].AsUInt32();
+	uint32_t submeshIndex            = flexVec[1].AsUInt32();
 
 	if(model != nullptr)
 		model->replaceMaterial(submeshIndex, panelOriginal->surface->surfaceMatAlphaBlend);
