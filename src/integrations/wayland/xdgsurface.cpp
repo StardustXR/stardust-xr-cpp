a3dbcb0fec1c36287c37004c53295f39d2b848d8
Nova <technobaboo@gmail.com>
2022-04-15 19:25:36 -0400

    Set wlroots to version 0.15.1

--- src/integrations/wayland/xdgsurface.cpp
+++ src/integrations/wayland/xdgsurface.cpp
@@ -11,18 +11,18 @@ XDGSurface::XDGSurface(wl_display *display, wlr_renderer *renderer, wlr_xdg_surf
 	if(wlr_surface_is_xdg_surface(surface))
 		printf("New XDG surface created\n");
 
-	wlr_xdg_toplevel_set_activated(xdg_surface->toplevel, true);
+	wlr_xdg_toplevel_set_activated(xdg_surface, true);
 	// wlr_xdg_toplevel_set_fullscreen(xdg_surface, true);
-	wlr_xdg_toplevel_set_maximized(xdg_surface->toplevel, true);
-	wlr_xdg_toplevel_set_tiled(xdg_surface->toplevel, WLR_EDGE_TOP | WLR_EDGE_BOTTOM | WLR_EDGE_RIGHT | WLR_EDGE_LEFT);
+	wlr_xdg_toplevel_set_maximized(xdg_surface, true);
+	wlr_xdg_toplevel_set_tiled(xdg_surface, WLR_EDGE_TOP | WLR_EDGE_BOTTOM | WLR_EDGE_RIGHT | WLR_EDGE_LEFT);
 }
 
 void XDGSurface::resize(uint32_t width, uint32_t height) const {
-	wlr_xdg_toplevel_set_size(xdg_surface->toplevel, width, height);
+	wlr_xdg_toplevel_set_size(xdg_surface, width, height);
 }
 
 void XDGSurface::close() const {
-	wlr_xdg_toplevel_send_close(xdg_surface->toplevel);
+	wlr_xdg_toplevel_send_close(xdg_surface);
 }
 
 bool XDGSurface::isMapped() const {
