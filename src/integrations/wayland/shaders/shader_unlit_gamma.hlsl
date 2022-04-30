cb9c74f96ae1af92c12a6b934fce584e6acf6c83
Nova <technobaboo@gmail.com>
2022-04-29 22:37:06 -0400

    Remove color property from wayland shader gamma

--- src/integrations/wayland/shaders/shader_unlit_gamma.hlsl
+++ src/integrations/wayland/shaders/shader_unlit_gamma.hlsl
@@ -1,9 +1,7 @@
 #include "stereokit.hlsli"
 
 //--name = sk/unlit
-//--color:color = 1, 1, 1, 1
 //--diffuse     = white
-float4       color;
 Texture2D    diffuse   : register(t0);
 SamplerState diffuse_s : register(s0);
 
@@ -11,12 +9,10 @@ struct vsIn {
 	float4 pos  : SV_Position;
 	float3 norm : NORMAL0;
 	float2 uv   : TEXCOORD0;
-	float4 col  : COLOR0;
 };
 struct psIn {
 	float4 pos   : SV_POSITION;
 	float2 uv    : TEXCOORD0;
-	float4 color : COLOR0;
 	uint view_id : SV_RenderTargetArrayIndex;
 };
 
@@ -29,15 +25,11 @@ psIn vs(vsIn input, uint id : SV_InstanceID) {
 	o.pos        = mul(float4(world,         1), sk_viewproj[o.view_id]);
 
 	o.uv    = input.uv;
-	o.color = input.col * color * sk_inst[id].color;
 	return o;
 }
 float4 ps(psIn input) : SV_TARGET {
-	float gamma = 2.2;
 	float4 col = diffuse.Sample(diffuse_s, input.uv);
-
-	col = col * input.color;
-	col.rgb = pow(col.rgb, float3(gamma));
+	col.rgb = pow(col.rgb, float3(2.2));
 
 	return col; 
 }
